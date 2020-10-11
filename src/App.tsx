0f14178b4ab2bc8b101083df27cdfd3a0a9ef5df
colinshovlin <cshovplaysultimate@gmail.com>
2020-10-11 16:16:26 -0400

    updating movies JSON, updating formatting, and adding navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,62 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
+
 import './App.css';
 
 import MoviesList from './components/MoviesList/MoviesList';
 import BingeStats from "./components/BingeStats/BingeStats";
+import MoviesByYear from "./components/moviesByYear/MoviesByYear";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-        <header className="App-header">
-          <h1>Horror Movie Binge 2020</h1>
-          <p>Goal is to hit 365 for the year</p>
-        </header>
-        <div>
-            <BingeStats/>
-            <hr/>
-            <h3>Movies watched in chronological order:</h3>
-            <MoviesList />
-        </div>
-    </div>
+      <Router>
+          <div className="App">
+              <header className="App-header">
+                  <h1>Horror Movie Binge 2020</h1>
+                  <p>Goal is to hit 365 for the year</p>
+              </header>
+              <nav className="navMenu">
+                  <Link to="/">Home</Link>
+                  <Link to="/movies-by-year">Year totals</Link>
+                  <Link to="/stats">Stats</Link>
+              </nav>
+
+              <Switch>
+
+                  <Route path="/stats">
+                      <BingeStats></BingeStats>
+                  </Route>
+                  <Route path="/movies-by-year">
+                      <MoviesByYear></MoviesByYear>
+                  </Route>
+                  <Route path="/">
+                      <MoviesList></MoviesList>
+                  </Route>
+              </Switch>
+
+          </div>
+
+
+
+
+      </Router>
+
+
+    // <div className="App">
+    //     {/*<Link to="/">Home</Link>*/}
+    //     <header className="App-header">
+    //       <h1>Horror Movie Binge 2020</h1>
+    //       <p>Goal is to hit 365 for the year</p>
+    //         <Link to={"/"}></Link>
+    //     </header>
+    //     <div>
+    //         <Route path="/" component={MoviesList}></Route>
+    //         <Route path="/stats" component={BingeStats}></Route>
+    //         {/*<BingeStats/>*/}
+    //         {/*<hr/>*/}
+    //         {/*<h3>Movies watched in chronological order:</h3>*/}
+    //         {/*<MoviesList />*/}
+    //     </div>
+    // </div>
   );
 }
-
-export default App;
--- src/components/BingeStats/BingeStats.tsx
+++ src/components/BingeStats/BingeStats.tsx
@@ -1,57 +1,82 @@
 import {getDaysLeft, getNumberMovies, getNumberMoviesToReachGoal, getDecadeCounts} from "../../services/movieListHelpers";
 import React from "react";
 
+import './BingeStats.css';
+
 function BingeStats() {
     return (
-            <div>
+            <div className="bingeStats">
 
-                <h3>Movies So Far</h3>
-                <p>{getNumberMovies()}</p>
+                <div className="statRow">
+                    <div className="statContainer">
+                        <h3>Movies So Far</h3>
+                        <p>{getNumberMovies()}</p>
+                    </div>
+                    <div className="statContainer">
+                        <h3>Days Left</h3>
+                        <p>{getDaysLeft()}</p>
+                    </div>
+                    <div className="statContainer">
+                        <h3>Movies/Day to Reach Goal</h3>
+                        <p>{getNumberMoviesToReachGoal()}</p>
+                    </div>
+                </div>
 
-                <h3>Days Left</h3>
-                <p>{getDaysLeft()}</p>
+                <div className="statRow">
+                    <div className="statContainer">
+                        <h3>1900</h3>
+                        <p>{getDecadeCounts()['1900']}</p>
+                    </div>
+                    <div className="statContainer">
+                        <h3>1910</h3>
+                        <p>{getDecadeCounts()['1910']}</p>
+                    </div>
+                    <div className="statContainer">
+                        <h3>1920</h3>
+                        <p>{getDecadeCounts()['1920']}</p>
+                    </div>
+                    <div className="statContainer">
+                        <h3>1930</h3>
+                        <p>{getDecadeCounts()['1930']}</p>
+                    </div>
+                    <div className="statContainer">
+                        <h3>1940</h3>
+                        <p>{getDecadeCounts()['1940']}</p>
+                    </div>
+                    <div className="statContainer">
+                        <h3>1950</h3>
+                        <p>{getDecadeCounts()['1950']}</p>
+                    </div>
+                    <div className="statContainer">
+                        <h3>1960</h3>
+                        <p>{getDecadeCounts()['1960']}</p>
+                    </div>
+                    <div className="statContainer">
+                        <h3>1970</h3>
+                        <p>{getDecadeCounts()['1970']}</p>
+                    </div>
+                    <div className="statContainer">
+                        <h3>1980</h3>
+                        <p>{getDecadeCounts()['1980']}</p>
+                    </div>
+                    <div className="statContainer">
+                        <h3>1990</h3>
+                        <p>{getDecadeCounts()['1990']}</p>
+                    </div>
+                    <div className="statContainer">
+                        <h3>2000</h3>
+                        <p>{getDecadeCounts()['2000']}</p>
+                    </div>
+                    <div className="statContainer">
+                        <h3>2010</h3>
+                        <p>{getDecadeCounts()['2010']}</p>
+                    </div>
+                    <div className="statContainer">
+                        <h3>2020</h3>
+                        <p>{getDecadeCounts()['2020']}</p>
+                    </div>
 
-                <h3>Movies/Day to Reach Goal</h3>
-                <p>{getNumberMoviesToReachGoal()}</p>
-
-                <hr/>
-
-                <table>
-                    <tr>
-                        <th>1900</th>
-                        <th>1910</th>
-                        <th>1920</th>
-                        <th>1930</th>
-                        <th>1940</th>
-                        <th>1950</th>
-                        <th>1960</th>
-                        <th>1970</th>
-                        <th>1980</th>
-                        <th>1990</th>
-                        <th>2000</th>
-                        <th>2010</th>
-                        <th>2020</th>
-                    </tr>
-                    <tr>
-                        <td>{getDecadeCounts()['1900']}</td>
-                        <td>{getDecadeCounts()['1910']}</td>
-                        <td>{getDecadeCounts()['1920']}</td>
-                        <td>{getDecadeCounts()['1930']}</td>
-                        <td>{getDecadeCounts()['1940']}</td>
-                        <td>{getDecadeCounts()['1950']}</td>
-                        <td>{getDecadeCounts()['1960']}</td>
-                        <td>{getDecadeCounts()['1970']}</td>
-                        <td>{getDecadeCounts()['1980']}</td>
-                        <td>{getDecadeCounts()['1990']}</td>
-                        <td>{getDecadeCounts()['2000']}</td>
-                        <td>{getDecadeCounts()['2010']}</td>
-                        <td>{getDecadeCounts()['2020']}</td>
-
-
-
-                    </tr>
-                </table>
-
+                </div>
 
             </div>
     );
